af9ee5817c0dcc92f0fa8605ba213be66d727270
Shannon Skipper <shannonskipper@server.local>
2012-11-24 23:34:11 -0800

    make serializer optional

--- lib/bindi.rb
+++ lib/bindi.rb
@@ -1,56 +1,58 @@
 require 'ohm'
 require 'bindi/version'
 
-module Bindi
-  class << self
-    def connect
-      Ohm.connect
-    end
+class Bindi
+  def initialize serializer
+    @serializer = serializer
+  end
+  
+  def connect
+    Ohm.connect
+  end
 
-    def [] key
-      Marshal.load(Ohm.redis.get key)
-    end
+  def [] key
+    @serializer.load(Ohm.redis.get key)
+  end
 
-    def []= key, value
-      Ohm.redis.set key, Marshal.dump(value)
-    end
+  def []= key, value
+    Ohm.redis.set key, @serializer.dump(value)
+  end
 
-    def clear
-      Ohm.redis.flushdb
-    end
+  def clear
+    Ohm.redis.flushdb
+  end
 
-    def delete key
-      Ohm.redis.del key
-    end
+  def delete key
+    Ohm.redis.del key
+  end
 
-    def each
-      Ohm.redis.keys.each do
-        yield
-      end
+  def each
+    Ohm.redis.keys.each do
+      yield
     end
+  end
 
-    def empty?
-      Ohm.redis.keys.empty?
-    end
+  def empty?
+    Ohm.redis.keys.empty?
+  end
 
-    def info
-      Ohm.redis.info
-    end
+  def info
+    Ohm.redis.info
+  end
     
-    def inspect
-      Ohm.redis.inspect
-    end
+  def inspect
+    Ohm.redis.inspect
+  end
 
-    alias to_s inspect
+  alias to_s inspect
 
-    def key? key
-      Ohm.redis.exists key
-    end
+  def key? key
+    Ohm.redis.exists key
+  end
 
-    alias has_key key?
+  alias has_key key?
 
-    def keys
-      Ohm.redis.keys.map &:to_sym
-    end
+  def keys
+    Ohm.redis.keys.map &:to_sym
   end
 end
